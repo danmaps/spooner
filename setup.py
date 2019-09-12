98d4e500887cae02e04c3977670b594de5303cc8
Danny McVey <dmcvey@esri.com>
2019-09-12 15:11:33 -0700

    bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 from setuptools import setup, find_packages
 
-version = '0.5.4'
+version = '0.5.5'
 
 with open('README.md') as readme_file:
     readme = readme_file.read()
--- spooner/__init__.py
+++ spooner/__init__.py
@@ -1,6 +1,6 @@
 """spooner"""
 
-__version__ = "0.5.4"
+__version__ = "0.5.5"
 
 
 from .spooner import phonemes  # noqa
